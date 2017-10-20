aecdbedce1414fc1e7b228c1fe97542c1df345ff
Vinicius Po <po.paes.vinicius@gmail.com>
2017-10-20 22:29:19 +0000

    Aula

--- js/Controller/telaRestauranteController.js
+++ js/Controller/telaRestauranteController.js
@@ -7,71 +7,107 @@ $scope.idRestaurante = $location.search().idRestaurante;
 
 $scope.getShopInformation = function(){
     
-    $http({
-      type: 'GET',
-      url: 'phps/info_restaurante.php',
-      params : {idUsuario:$scope.idUsuario,
-                idRestaurante:$scope.idRestaurante
-      },
-      dataType:'json'
-    }).then(function (response) {
-        
-        console.log(response);
-        
-        $scope.nomeRestaurante = response.data.records[0].nomeRestaurante;
-        $scope.imageRestaurante = response.data.records[0].imageRestaurante.replace(/(\r\n|\n|\r)/gm,"");
-        $scope.enderecoRestaurante = response.data.records[0].enderecoRestaurante;
-        $scope.latitudeRestaurante = response.data.records[0].latitudeRestaurante;
-        $scope.longitudeRestaurante = response.data.records[0].longitudeRestaurante;
-        $scope.numeroPontosRestaurante = response.data.records[0].numeroPontosRestaurante;
-        $scope.pontosUsuario = response.data.records[0].pontosUsuario;
-        
-        $scope.numberOfCircles = [];
-        
-        for (var i = 0; i < $scope.numeroPontosRestaurante; i++) { 
+    if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(showPosition);
+    
+    
+    function showPosition(position) {
+        $scope.latitude = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+    
+        $http({
+          type: 'GET',
+          url: 'phps/info_restaurante.php',
+          params : {idUsuario:$scope.idUsuario,
+                    idRestaurante:$scope.idRestaurante
+          },
+          dataType:'json'
+        }).then(function (response) {
             
-            if(i < $scope.pontosUsuario){
-                $scope.numberOfCircles[i] = {
-                    "classe" : "ponto-marcado"
-                }
+            console.log(response);
+            
+            $scope.nomeRestaurante = response.data.records[0].nomeRestaurante;
+            $scope.imageRestaurante = response.data.records[0].imageRestaurante.replace(/(\r\n|\n|\r)/gm,"");
+            $scope.enderecoRestaurante = response.data.records[0].enderecoRestaurante;
+            $scope.latitudeRestaurante = response.data.records[0].latitudeRestaurante;
+            $scope.longitudeRestaurante = response.data.records[0].longitudeRestaurante;
+            $scope.numeroPontosRestaurante = response.data.records[0].numeroPontosRestaurante;
+            $scope.pontosUsuario = response.data.records[0].pontosUsuario;
+            
+            
+            if($scope.nomeRestaurante == "Shades Studio"){
+                $scope.pdfEstabelecimento = "/pdf/salao.pdf";
             }else{
-                 $scope.numberOfCircles[i] = {
-                    "classe" : "ponto-nao-marcado"
-                }
+                $scope.pdfEstabelecimento = "/pdf/restaurante.pdf";
             }
             
             
             
-        } 
+            $scope.numberOfCircles = [];
+            
+            for (var i = 0; i < $scope.numeroPontosRestaurante; i++) { 
+                
+                if(i < $scope.pontosUsuario){
+                    $scope.numberOfCircles[i] = {
+                        "classe" : "ponto-marcado"
+                    }
+                }else{
+                     $scope.numberOfCircles[i] = {
+                        "classe" : "ponto-nao-marcado"
+                    }
+                }
+                
+                
+                
+            } 
+            
+            
+            $scope.cardBack = {
+                "background": "url('"+$scope.imageRestaurante+"') no-repeat center"
+            }
+            
+            $scope.widthBolhasCartao = {
+                "width" : ""+(200/$scope.numeroPontosRestaurante)+"%"
+            }
+            
+            
+            
+            
+            myMap();
+            $scope.isLoading = false;
         
-        
-        $scope.cardBack = {
-            "background": "url('"+$scope.imageRestaurante+"') no-repeat center"
-        }
-        
-        $scope.widthBolhasCartao = {
-            "width" : ""+(200/$scope.numeroPontosRestaurante)+"%"
-        }
-        
-        
-        
-        
-        myMap();
-        $scope.isLoading = false;
-    });
+        });
+    }
 };
 
 
 function myMap() {
+    
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
     var mapOptions = {
         center: new google.maps.LatLng($scope.latitudeRestaurante, $scope.longitudeRestaurante),
         zoom: 16,
         mapTypeId: 'roadmap'
     }
+    
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var marker = new google.maps.Marker({
-      position: {lat: Number($scope.latitudeRestaurante), lng: Number($scope.longitudeRestaurante)},
-      map: map
+    directionsDisplay.setMap(map);
+    //var marker = new google.maps.Marker({
+     // position: {lat: Number($scope.latitudeRestaurante), lng: Number($scope.longitudeRestaurante)},
+      //map: map
+    //});
+    
+    directionsService.route({
+      origin: {lat: Number($scope.latitude), lng: Number($scope.longitude)},
+      destination: {lat: Number($scope.latitudeRestaurante), lng: Number($scope.longitudeRestaurante)},
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
     });
 }
 
